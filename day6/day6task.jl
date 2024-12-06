2cb760fac8829e4de5835a84a078fe5f221e873e
markus <bandor@posteo.de>
2024-12-06 23:52:31 +0100

    Not complete solution for day6
    I really like my solution for part1. I like the solution for part2 as
    well, but i does not work. Maybe I have to reread the instructions or I
    am just missing something.
    But it was fun and I learned a lot about the language. especially about
    Matrices.

--- day6/day6task.jl
+++ day6/day6task.jl
@@ -1,76 +1,131 @@
 function read_my_file(filepath::String)
-    file_content = open(filepath, "r") do file
-        read(file, String)
-    end
-    return file_content
+        file_content = open(filepath, "r") do file
+                read(file, String)
+        end
+        return file_content
 end
 
 function convert_to_matrix(map::String)
-    lines = split(map, '\n')
-	#map_m = [line for line in lines]
-	map_m = [[Int(c) for c in line] for line in lines]
-	# 46 = .
-	# 35 = #
-	# 60 = <
-	# 62 = >
-	# 94 = ^
-	# 118 = v
-	map_m = reduce(hcat, map_m[1:end-1])
-    return map_m
+        lines = split(map, '\n')
+        # map_m = [line for line in lines]
+        map_m = [[Int(c) for c in line] for line in lines]
+        # 46 = .
+        # 35 = #
+        # 60 = <
+        # 62 = >
+        # 94 = ^
+        # 118 = v
+        # 79 = O
+        map_m = reduce(vcat, map_m[1:end-1]')
+        return map_m
 end
 
-function find_guard(map::Matrix{Int}) 
-	if (pos = findfirst(isequal(60), map)) != nothing
-		println("Guard is facing left")
-		return findfirst(isequal(62), rot180(map)), rot180(map)
-	elseif (pos = findfirst(isequal(62), map)) != nothing
-		println("Guard is facing right")
-		return pos, map
-	elseif (pos = findfirst(isequal(94), map)) != nothing
-		println("Guard is facing up")
-		return findfirst(isequal(94), rotr90(map)), rotr90(map)
-	elseif (pos = findfirst(isequal(116), map)) != nothing
-		println("Guard is facing down")
-		return findfirst(isequal(116), rotl90(map)), rotl90(map)
-	end
+function find_guard(map::Matrix{Int})
+        if (pos = findfirst(isequal(60), map)) !== nothing
+                println("Guard is facing left")
+                return findfirst(isequal(62), rot180(map)), rot180(map)
+        elseif (pos = findfirst(isequal(62), map)) !== nothing
+                println("Guard is facing right")
+                return pos, map
+        elseif (pos = findfirst(isequal(94), map)) !== nothing
+                println("Guard is facing up")
+                return findfirst(isequal(94), rotr90(map)), rotr90(map)
+        elseif (pos = findfirst(isequal(116), map)) !== nothing
+                println("Guard is facing down")
+                return findfirst(isequal(116), rotl90(map)), rotl90(map)
+        end
 end
 
 function move_guard(map::Matrix{Int}, pos::CartesianIndex{2})
-	map[pos] = 88	
-	#Are we at the end?
-	if pos[1]+1 > size(map)[1]
-		println("End")
-		return map, CartesianIndex(-1, -1)
-	# Is there an obstacle?
-	elseif map[pos[1]+1, pos[2]] == 35
-		# Rotate map & return new position
-		# Because we transpose the map, we need to flip rows & cols
-		return rotl90(map), CartesianIndex(pos[2], pos[1] + 1)
-	# Move on
-	else
-		return map, CartesianIndex(pos[1] + 1, pos[2])
-	end
+        map[pos] = 88
+        #Are we at the end?
+        if pos[2] + 1 > size(map)[1] # Exit map
+                return map, CartesianIndex(-1, -1), 0
+                # Is there an obstacle?
+        elseif map[pos[1], pos[2]+1] == 35 # Blocked
+                # Rotate map & return new position
+                # Because we transpose the map, we need to flip rows & cols
+                return rotl90(map), CartesianIndex(131 - pos[2], pos[1] + 1), 1
+        elseif map[pos[1], pos[2]+1] == 79 # Obstacle
+                # Rotate map & return new position
+                # Because we transpose the map, we need to flip rows & cols
+                return rotl90(map), CartesianIndex(131 - pos[2], pos[1] + 1), 2
+        else # Move on
+                new_pos = CartesianIndex(pos[1], pos[2] + 1)
+                map[new_pos] = 94
+                return map, new_pos, 3
+        end
 end
 
-begin
-	# Get the map
-	filepath_map = "input_day6.txt"
-	input_map = read_my_file(filepath_map)
+# We will walk the guard through the corse step by step
+function walk_guard(map::Matrix{Int}, pos::CartesianIndex{2})
+        step_cnt = 0
+        watchdog = 0
+        obstacle_cnt = 0
+        while true
+                map, pos, state = move_guard(map, pos)
+                if state == 2
+                        obstacle_cnt = obstacle_cnt + 1
+                        if obstacle_cnt == 2
+                                print("loop ")
+                                return 1
+                        end
+                end
+                step_cnt = step_cnt + 1
+                if pos[1] == -1
+                        break
+                end
+                watchdog = watchdog + 1
+                if watchdog == 15000
+                        println("watchdog")
+                        return 2
+                end
+        end
+        # println(step_cnt)
+        # println(map)
+        return 0
+end
 
-	# Convert to Matrix
-	map = convert_to_matrix(input_map)
-	pos_guard, map= find_guard(map)
-	println("Guard position (col, row)")
-	println(pos_guard[1])
-	println(pos_guard[2])
-	step_cnt = 0
-	while pos_guard[1] != -1
-		map, pos_guard = move_guard(map, pos_guard)
-		step_cnt = step_cnt + 1
-	end
-	println("Steps the guard walked")
-	println(step_cnt)
-	blocked = findall(isequal(88), map)
-	println("Blocked postitions")
-	println(length(blocked))
+# Place the obstacle at any possible point
+function place_obstacle(map::Matrix{Int}, pos::CartesianIndex{2}, obstacle::CartesianIndex{2})
+        if map[obstacle] == 46 # We can place on empty spots only
+                map[obstacle] = 79
+                if walk_guard(map, pos) == 1 # We are stuck in a loop, which is what we want
+                        println(obstacle)
+                        return true
+                end
+                return false
+        end
+        return false
+end
+
+function main()
+        # Get the map
+        filepath_map = "input_day6"
+        input_map = read_my_file(filepath_map)
+
+        # Convert to Matrix
+        map = convert_to_matrix(input_map)
+        pos_guard, map = find_guard(map)
+        println("Guard position (row, col)")
+        println(pos_guard[1])
+        println(pos_guard[2])
+        walk_guard(map, pos_guard)
+
+        blocked = findall(isequal(88), map)
+        println("Blocked postitions")
+        println(length(blocked))
+
+        cnt = 0
+        for row in 1:size(map, 1), col in 1:size(map, 2)
+                if place_obstacle(map, pos_guard, CartesianIndex(row, col))
+                        cnt = cnt + 1
+                end
+        end
+        println(cnt)
+end
+
+# Program entry point
+if abspath(PROGRAM_FILE) == @__FILE__
+        main()
 end
