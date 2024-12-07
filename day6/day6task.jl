e8dfe0a8a2e5a633ba9227b3b3151586cedb4f66
markus <bandor@posteo.de>
2024-12-07 14:39:24 +0100

    I dont really like this
    I[32;2ulearned a lot, but I dont like my solution now

--- day6/day6task.jl
+++ day6/day6task.jl
@@ -67,8 +67,7 @@ function walk_guard(map::Matrix{Int}, pos::CartesianIndex{2})
                 map, pos, state = move_guard(map, pos)
                 if state == 2
                         obstacle_cnt = obstacle_cnt + 1
-                        if obstacle_cnt == 5
-                                # print("loop ")
+                        if obstacle_cnt == 2000
                                 return map, 1
                         end
                 end
@@ -77,7 +76,7 @@ function walk_guard(map::Matrix{Int}, pos::CartesianIndex{2})
                         break
                 end
                 watchdog = watchdog + 1
-                if watchdog == 10000
+                if watchdog == 30000
                         println("watchdog $obstacle_cnt")
                         return map, 2
                 end
@@ -92,8 +91,8 @@ function place_obstacle(map::Matrix{Int}, map_blocked::Matrix{Int}, pos::Cartesi
         if map_blocked[obstacle] == 88 && obstacle !== pos # We will place only, where the guard walks, but not at the start
                 map[obstacle] = 79
                 map, state = walk_guard(map, pos)
-                # if state == 2 # We are stuck in a loop, which is what we want
-                if state == 1 || state == 2 # We are stuck in a loop, which is what we want
+                if state == 1 # We are stuck in a loop, which is what we want
+                        # if state == 1 || state == 2 # We are stuck in a loop, which is what we want
                         println(obstacle)
                         return 1
                 end
@@ -128,24 +127,34 @@ function main()
 
         # Part2, where we place obstacels
         cnt = 0
+        counter = Threads.Atomic{Int}(0)
+        skip_counter = Threads.Atomic{Int}(0)
+        used_counter = Threads.Atomic{Int}(0)
         skip_cnt = 0
         used_cnt = 0
         runtime = @elapsed begin
+                # state = place_obstacle(map, map_blocked, pos_guard, CartesianIndex(1, 1))
+                # println(map)
                 for row in 1:size(map, 1)
-                        # Threads.@threads for col in 1:size(map, 2)
-                        for col in 1:size(map, 2)
+                        Threads.@threads for col in 1:size(map, 2)
+                                # for col in 1:size(map, 2)
                                 state = place_obstacle(map, map_blocked, pos_guard, CartesianIndex(row, col))
                                 if state == 1
                                         cnt = cnt + 1
+                                        Threads.atomic_add!(counter, 1)
                                 elseif state == 3
                                         skip_cnt = skip_cnt + 1
+                                        Threads.atomic_add!(skip_counter, 1)
                                 else
                                         used_cnt = used_cnt + 1
+                                        Threads.atomic_add!(used_counter, 1)
                                 end
                         end
                 end
         end
+        println("obstacle, skipped, used")
         println("$cnt, $skip_cnt, $used_cnt")
+        println("$counter, $skip_counter, $used_counter")
         println("Elapsed time: $runtime seconds")
 end
 
