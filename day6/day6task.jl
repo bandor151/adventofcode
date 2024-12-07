fb4442a625ae240f1921211952a55ddec326c8af
markus <bandor@posteo.de>
2024-12-07 12:05:56 +0100

    optimizing code and preparing multi threading
    This did not help with the solution, but is really cool.
    There are still race conditions

--- day6/day6task.jl
+++ day6/day6task.jl
@@ -15,6 +15,7 @@ function convert_to_matrix(map::String)
         # 62 = >
         # 94 = ^
         # 118 = v
+        # 88 = X
         # 79 = O
         map_m = reduce(vcat, map_m[1:end-1]')
         return map_m
@@ -46,7 +47,7 @@ function move_guard(map::Matrix{Int}, pos::CartesianIndex{2})
                 # Rotate map & return new position
                 # Because we transpose the map, we need to flip rows & cols
                 return rotl90(map), CartesianIndex(131 - pos[2], pos[1] + 1), 1
-        elseif map[pos[1], pos[2]+1] == 79 # Obstacle
+        elseif map[pos[1], pos[2]+1] == 79 # Obstacle = Blocked
                 # Rotate map & return new position
                 # Because we transpose the map, we need to flip rows & cols
                 return rotl90(map), CartesianIndex(131 - pos[2], pos[1] + 1), 2
@@ -66,63 +67,86 @@ function walk_guard(map::Matrix{Int}, pos::CartesianIndex{2})
                 map, pos, state = move_guard(map, pos)
                 if state == 2
                         obstacle_cnt = obstacle_cnt + 1
-                        if obstacle_cnt == 2
-                                print("loop ")
-                                return 1
+                        if obstacle_cnt == 5
+                                # print("loop ")
+                                return map, 1
                         end
                 end
                 step_cnt = step_cnt + 1
-                if pos[1] == -1
+                if state == 0
                         break
                 end
                 watchdog = watchdog + 1
-                if watchdog == 15000
-                        println("watchdog")
-                        return 2
+                if watchdog == 10000
+                        println("watchdog $obstacle_cnt")
+                        return map, 2
                 end
         end
         # println(step_cnt)
         # println(map)
-        return 0
+        return map, 0
 end
 
 # Place the obstacle at any possible point
-function place_obstacle(map::Matrix{Int}, pos::CartesianIndex{2}, obstacle::CartesianIndex{2})
-        if map[obstacle] == 46 # We can place on empty spots only
+function place_obstacle(map::Matrix{Int}, map_blocked::Matrix{Int}, pos::CartesianIndex{2}, obstacle::CartesianIndex{2})
+        if map_blocked[obstacle] == 88 && obstacle !== pos # We will place only, where the guard walks, but not at the start
                 map[obstacle] = 79
-                if walk_guard(map, pos) == 1 # We are stuck in a loop, which is what we want
+                map, state = walk_guard(map, pos)
+                # if state == 2 # We are stuck in a loop, which is what we want
+                if state == 1 || state == 2 # We are stuck in a loop, which is what we want
                         println(obstacle)
-                        return true
+                        return 1
                 end
-                return false
+                return 2
         end
-        return false
+        return 3
 end
 
 function main()
+        # Check how many threads we allow
+        print("Number of Threads:")
+        println(Threads.nthreads())
+
         # Get the map
         filepath_map = "input_day6"
         input_map = read_my_file(filepath_map)
 
         # Convert to Matrix
         map = convert_to_matrix(input_map)
-        pos_guard, map = find_guard(map)
-        println("Guard position (row, col)")
-        println(pos_guard[1])
-        println(pos_guard[2])
-        walk_guard(map, pos_guard)
 
-        blocked = findall(isequal(88), map)
-        println("Blocked postitions")
+        # Part1, where we find the blocked spots
+        runtime = @elapsed begin
+                pos_guard, map = find_guard(map)
+        end
+        println("Guard position (row, col) ($pos_guard[1], $pos_guard[2])")
+        map_blocked, state = walk_guard(map, pos_guard)
+
+        blocked = findall(isequal(88), map_blocked)
+        print("Blocked postitions: ")
         println(length(blocked))
+        println("Elapsed time: $runtime seconds")
 
+        # Part2, where we place obstacels
         cnt = 0
-        for row in 1:size(map, 1), col in 1:size(map, 2)
-                if place_obstacle(map, pos_guard, CartesianIndex(row, col))
-                        cnt = cnt + 1
+        skip_cnt = 0
+        used_cnt = 0
+        runtime = @elapsed begin
+                for row in 1:size(map, 1)
+                        # Threads.@threads for col in 1:size(map, 2)
+                        for col in 1:size(map, 2)
+                                state = place_obstacle(map, map_blocked, pos_guard, CartesianIndex(row, col))
+                                if state == 1
+                                        cnt = cnt + 1
+                                elseif state == 3
+                                        skip_cnt = skip_cnt + 1
+                                else
+                                        used_cnt = used_cnt + 1
+                                end
+                        end
                 end
         end
-        println(cnt)
+        println("$cnt, $skip_cnt, $used_cnt")
+        println("Elapsed time: $runtime seconds")
 end
 
 # Program entry point
